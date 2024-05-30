2f79ad5f8295fd7e626dab78d11e61d6ac754eb8
bifinbabu <bifinbabu088@gmail.com>
2024-05-30 09:32:54 +0530

    Initial commit, DTO validation started

---
+++ src/users/users.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Injectable()
+export class UsersService {
+  private users = [
+    { id: 1, name: 'Dave', email: 'dave@gmail.com', role: 'ADMIN' },
+    { id: 2, name: 'sgdhj', email: 'dave2@gmail.com', role: 'INTERN' },
+    { id: 3, name: 'sfgg', email: 'dave3@gmail.com', role: 'INTERN' },
+    { id: 4, name: 'sdgsf', email: 'dave4@gmail.com', role: 'ENGINEER' },
+    { id: 5, name: 'sdgd', email: 'dave5@gmail.com', role: 'ENGINEER' },
+  ];
+
+  findAll(role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    if (role) {
+      return this.users.filter((user) => user.role === role);
+    }
+    return this.users;
+  }
+
+  findOne(id: number) {
+    const user = this.users.find((user) => user.id === id);
+    return user;
+  }
+
+  create(createUserDto: CreateUserDto) {
+    const usersByHighestId = [...this.users].sort((a, b) => b.id - a.id);
+    const newUser = {
+      id: usersByHighestId[0].id + 1,
+      ...createUserDto,
+      //   name: user.name,
+      //   email: user.email,
+      //   role: user.role,
+    };
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    this.users = this.users.map((user) => {
+      if (user.id === id) {
+        return { ...user, ...updateUserDto };
+      }
+      return user;
+    });
+    return this.findOne(id);
+  }
+
+  delete(id: number) {
+    const removedUser = this.findOne(id);
+    this.users = this.users.filter((user) => user.id !== id);
+    return removedUser;
+  }
+}
